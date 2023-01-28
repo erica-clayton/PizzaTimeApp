026a77bc9f641a4edf513caa56c1667430d26d41
Erica Clayton <erica.baertsch@gmail.com>
2023-01-28 12:46:33 -0600

    Add project files.

---
+++ PizzaTime/PizzaTopping.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaTime
+{
+    internal class PizzaToppings
+    {
+        public List<string> Toppings { get; set; }
+    }
+}
---
+++ PizzaTime/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using Newtonsoft.Json;
+using PizzaTime;
+
+var allPizzas = JsonConvert.DeserializeObject<List<PizzaToppings>>(File.ReadAllText(@"C:\Users\erica\E20\PizzaTimeApp\PizzaTime\Assests\pizzas.json"));
+
+Dictionary<string, int> pizzaCounter = new();
+
+foreach (PizzaToppings pizza in allPizzas)
+{
+    var toppingsAsString = String.Join(",", pizza.Toppings);
+    if(pizzaCounter.ContainsKey(toppingsAsString))
+    {
+        pizzaCounter[toppingsAsString] += 1;
+    }
+    else
+    {
+        pizzaCounter[toppingsAsString] = 1;
+    }
+}
+
+var mostCOmmonPizza = pizzaCounter.OrderByDescending(p => p.Value).Take(20);
+
+foreach (var common in mostCOmmonPizza)
+{
+    Console.WriteLine($"The topping(s) {common.Key} is seen {common.Value} times");
+}
